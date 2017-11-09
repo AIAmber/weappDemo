0aa34e0c9d7702974ad8704e56a8575adb322233
leolee <lzlrjok@vip.qq.com>
2017-11-09 11:27:54 +0800

    cmsDemo-v0.1.9 20171109

--- demo/settings.py
+++ demo/settings.py
@@ -43,12 +43,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'demo.urls'
 
@@ -57,7 +60,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
         # ,
-        'DIRS': ['/templates/dist'],
+        'DIRS': ['appfront/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,5 +132,5 @@ STATIC_URL = '/static/'
 
 # Add for vuejs
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "templates/dist/static"),
+    os.path.join(BASE_DIR, "appfront/dist/static"),
 ]
--- demo/urls.py
+++ demo/urls.py
@@ -22,5 +22,5 @@ import cmsDemo.urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(cmsDemo.urls)),
-    url(r'^$/', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
